e68842f76e0f6f86eb0cffb5c7050d46a2418250
Casey Scarborough <caseyscarborough@gmail.com>
2015-06-14 16:09:47 -0400

    Add Mockito

--- src/test/java/jerseyandspring/rest/UserResourceTest.java
+++ src/test/java/jerseyandspring/rest/UserResourceTest.java
@@ -12,9 +12,12 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class UserResourceTest extends JerseyTest {
 
@@ -23,6 +26,9 @@ public class UserResourceTest extends JerseyTest {
 
   @Override
   protected Application configure() {
+    userDao = mock(UserDao.class);
+    userResource = new UserResource(userDao);
+
     ResourceConfig config = new ResourceConfig();
     config.registerInstances(userResource);
     return config;
@@ -35,6 +41,7 @@ public class UserResourceTest extends JerseyTest {
 
   @Test
   public void testClientStringResponse() {
+    when(userDao.getAllUsers()).thenReturn(testUserList());
     Response s = target().path("users").request().get(Response.class);
     assertEquals(s.readEntity(List.class).size(), 3);
   }
@@ -42,9 +49,19 @@ public class UserResourceTest extends JerseyTest {
   @Test
   public void testCreateUser() {
     UserDto testUserDto = new UserDto("Test", "User");
+    when(userDao.save(testUserDto)).thenReturn(testUserDto);
+
     Response r = target().path("users").request().post(Entity.entity(testUserDto, MediaType.APPLICATION_JSON));
     assertEquals(r.getStatus(), 201);
     assertEquals(r.readEntity(UserDto.class).toString(), testUserDto.toString());
+  }
+
+  private List<UserDto> testUserList() {
+    List<UserDto> users = new ArrayList<UserDto>();
+    users.add(new UserDto("Test", "User1"));
+    users.add(new UserDto("Test", "User2"));
+    users.add(new UserDto("Test", "User3"));
+    return users;
   }
 
 }
